93388690435c0db8fc4c04b8c23ae1efbd838a38
Ashley Smith <ash.mash.134@gmail.com>
2020-10-26 14:06:48 +1100

    Updated API call

--- happy-message/src/MessageUseEffect.js
+++ happy-message/src/MessageUseEffect.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const APIMessage = () => {
   const getMessage = async () => {
-    let raw = await fetch('http://api.quotable.io/random');
+    let raw = await fetch('https://api.quotable.io/random');
     let apiMessage = await raw.json();
     setMessage(apiMessage);
   };
@@ -12,7 +12,7 @@ const APIMessage = () => {
     <>
       <h2>This uses an API call to generate a message</h2>
       <button className='btn' onClick={getMessage}>
-        Get a Message with an API call
+        Get a Message With An API Call
       </button>
       <p>
         '{message.content}' - {message.author}
